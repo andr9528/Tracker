e232b6c7d8b1b0a2cd7dcae6dfb9ec48a414f13f
André Steenhoff Madsen <andre@steenhoff.dk>
2026-07-31 12:43:47 +0200

    feature: add initial database migration

---
+++ Shared/Tracker.Shared.Persistence/Migrations/20260731103922_InitialCreate.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tracker.Shared.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CurrencyRate",
+                columns: table => new
+                {
+                    CurrencyRateId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FromCurrency = table.Column<int>(type: "INTEGER", nullable: false),
+                    ToCurrency = table.Column<int>(type: "INTEGER", nullable: false),
+                    Rate = table.Column<double>(type: "REAL", nullable: false),
+                    Date = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    Version = table.Column<string>(type: "BLOB", rowVersion: true, nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    CreatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CurrencyRate", x => x.CurrencyRateId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dish",
+                columns: table => new
+                {
+                    DishId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Version = table.Column<string>(type: "BLOB", rowVersion: true, nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    CreatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dish", x => x.DishId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredient",
+                columns: table => new
+                {
+                    IngredientId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Version = table.Column<string>(type: "BLOB", rowVersion: true, nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    CreatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    InStock = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredient", x => x.IngredientId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PaymentType",
+                columns: table => new
+                {
+                    PaymentTypeId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Version = table.Column<string>(type: "BLOB", rowVersion: true, nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    CreatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentType", x => x.PaymentTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RecurringPayment",
+                columns: table => new
+                {
+                    RecurringPaymentId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Start = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    End = table.Column<DateOnly>(type: "TEXT", nullable: true),
+                    PaymentTemplateId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Version = table.Column<string>(type: "BLOB", rowVersion: true, nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    CreatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecurringPayment", x => x.RecurringPaymentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dinner",
+                columns: table => new
+                {
+                    DinnerId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Date = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    Notes = table.Column<string>(type: "TEXT", nullable: true),
+                    DishId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Version = table.Column<string>(type: "BLOB", rowVersion: true, nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    CreatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    IsTakeAway = table.Column<bool>(type: "INTEGER", nullable: false),
+                    HasLeftovers = table.Column<bool>(type: "INTEGER", nullable: false),
+                    LeftoversEnoughForDinner = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IsLeftovers = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dinner", x => x.DinnerId);
+                    table.ForeignKey(
+                        name: "FK_Dinner_Dish_DishId",
+                        column: x => x.DishId,
+                        principalTable: "Dish",
+                        principalColumn: "DishId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DishIngredient",
+                columns: table => new
+                {
+                    DishIngredientId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DishId = table.Column<int>(type: "INTEGER", nullable: false),
+                    IngredientId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Version = table.Column<string>(type: "BLOB", rowVersion: true, nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    CreatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DishIngredient", x => x.DishIngredientId);
+                    table.ForeignKey(
+                        name: "FK_DishIngredient_Dish_DishId",
+                        column: x => x.DishId,
+                        principalTable: "Dish",
+                        principalColumn: "DishId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DishIngredient_Ingredient_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredient",
+                        principalColumn: "IngredientId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payment",
+                columns: table => new
+                {
+                    PaymentId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Date = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    RecurringPaymentId = table.Column<int>(type: "INTEGER", nullable: true),
+                    Amount = table.Column<double>(type: "REAL", nullable: false),
+                    Currency = table.Column<int>(type: "INTEGER", nullable: false),
+                    PaymentTypeId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Version = table.Column<string>(type: "BLOB", rowVersion: true, nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    CreatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payment", x => x.PaymentId);
+                    table.ForeignKey(
+                        name: "FK_Payment_PaymentType_PaymentTypeId",
+                        column: x => x.PaymentTypeId,
+                        principalTable: "PaymentType",
+                        principalColumn: "PaymentTypeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Payment_RecurringPayment_RecurringPaymentId",
+                        column: x => x.RecurringPaymentId,
+                        principalTable: "RecurringPayment",
+                        principalColumn: "RecurringPaymentId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PaymentTemplate",
+                columns: table => new
+                {
+                    PaymentTemplateId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Amount = table.Column<double>(type: "REAL", nullable: false),
+                    Currency = table.Column<int>(type: "INTEGER", nullable: false),
+                    PaymentTypeId = table.Column<int>(type: "INTEGER", nullable: false),
+                    RecurringPaymentId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Version = table.Column<string>(type: "BLOB", rowVersion: true, nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    CreatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentTemplate", x => x.PaymentTemplateId);
+                    table.ForeignKey(
+                        name: "FK_PaymentTemplate_PaymentType_PaymentTypeId",
+                        column: x => x.PaymentTypeId,
+                        principalTable: "PaymentType",
+                        principalColumn: "PaymentTypeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PaymentTemplate_RecurringPayment_RecurringPaymentId",
+                        column: x => x.RecurringPaymentId,
+                        principalTable: "RecurringPayment",
+                        principalColumn: "RecurringPaymentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dinner_Date",
+                table: "Dinner",
+                column: "Date",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dinner_DishId",
+                table: "Dinner",
+                column: "DishId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DishIngredient_DishId",
+                table: "DishIngredient",
+                column: "DishId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DishIngredient_IngredientId",
+                table: "DishIngredient",
+                column: "IngredientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payment_PaymentTypeId",
+                table: "Payment",
+                column: "PaymentTypeId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payment_RecurringPaymentId",
+                table: "Payment",
+                column: "RecurringPaymentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaymentTemplate_PaymentTypeId",
+                table: "PaymentTemplate",
+                column: "PaymentTypeId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaymentTemplate_RecurringPaymentId",
+                table: "PaymentTemplate",
+                column: "RecurringPaymentId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CurrencyRate");
+
+            migrationBuilder.DropTable(
+                name: "Dinner");
+
+            migrationBuilder.DropTable(
+                name: "DishIngredient");
+
+            migrationBuilder.DropTable(
+                name: "Payment");
+
+            migrationBuilder.DropTable(
+                name: "PaymentTemplate");
+
+            migrationBuilder.DropTable(
+                name: "Dish");
+
+            migrationBuilder.DropTable(
+                name: "Ingredient");
+
+            migrationBuilder.DropTable(
+                name: "PaymentType");
+
+            migrationBuilder.DropTable(
+                name: "RecurringPayment");
+        }
+    }
+}
